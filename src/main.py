8322b5bd809f1d1d1d7eaa7f9a8b1c2d2fde8587
lookoutking <lookoutking@gmail.com>
2024-05-09 20:38:14 +0800

    feat: add resize function

--- src/main.py
+++ src/main.py
@@ -13,19 +13,35 @@ templates = Jinja2Templates(directory="templates")
 ROOT_DATA_PATH = "./data"
 PROFILING = True
 
+api = PyTessBaseAPI(path=f"{ROOT_DATA_PATH}/tessdata", lang="chi_tra")
+
 
 @app.get("/", response_class=HTMLResponse)
 async def main(request: Request):
     return templates.TemplateResponse("index.html", context={"request": request})
 
 
+def resize_image(image, size):
+    width, height = image.size
+
+    max_height = size
+    max_width = size
+    if width > height:
+        max_height = max_width * height // width
+    else:
+        max_width = max_height * width // height
+
+    resized_image = image.resize((max_width, max_height), Image.BICUBIC)
+    return resized_image.convert("L")
+
+
 @app.post("/upload/")
 async def upload_file(file: UploadFile = File(...), request: Request = None):
     contents = await file.read()
     image = Image.open(io.BytesIO(contents))
-    with PyTessBaseAPI(path=f"{ROOT_DATA_PATH}/tessdata", lang="chi_tra") as api:
-        api.SetImage(image)
-        message = api.GetUTF8Text()
+    resized = resize_image(image, 400)
+    api.SetImage(resized)
+    message = api.GetUTF8Text()
     # return JSONResponse(status_code=200, content={"message": f"File uploaded successfully: {message}"})
     return templates.TemplateResponse(
         "index.html", {"request": request, "message": message}
