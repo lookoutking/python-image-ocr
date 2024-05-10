6e7f98a90d94d333928bdc19de61756d7147ed19
WANLUN.TSAI 蔡菀倫 <WANLUN.TSAI@deltaww.com>
2024-05-10 10:34:42 +0800

    refactor: project structure

--- src/routes/prediction.py
+++ src/routes/prediction.py
@@ -6,12 +6,12 @@ from src.services.models import OcrRecognitionModel
 from src.schemas.prediction import OcrRecognitionResult
 
 router = APIRouter()
+model = OcrRecognitionModel()
 
 
 @router.post("/predict", response_model=OcrRecognitionResult, name="predict")
 async def post_predict(
     file: UploadFile = File(...), request: Request = None
 ) -> OcrRecognitionResult:
-    model = OcrRecognitionModel()
     prediction = await model.predict(file)
     return prediction
